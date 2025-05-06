7a5ccc461442444367e0c71b4007d2984ef3f179
Tooba-Baqai <toobabaqai1@gmail.com>
2025-05-06 16:33:28 +0500

    Changes done by Tooba

---
+++ client/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('Proxy error:', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending request to the target:', req.method, req.url);
+          });
+        },
+        rewrite: (path) => path
+      }
+    }
+  }
+}) 
